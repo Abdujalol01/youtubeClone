c9f6ab0a5219aafe973d90aed56fa4e996b80870
Abdujalol Xalimov <118599854+Abdujalol01@users.noreply.github.com>
2023-10-12 00:19:45 +0500

    Add files via upload

---
+++ src/constants/index.jsx
@@ -0,0 +1,32 @@
+import {
+  OndemandVideo,
+  Home,
+  MusicNote,
+  LiveTv,
+  School,
+  SportsEsports,
+  FitnessCenter,
+  TheaterComedy,
+  GraphicEq,
+  Checkroom,
+  FaceRetouchingNatural,
+  DeveloperMode,
+} from "@mui/icons-material";
+
+export const logo =
+  "https://upload.wikimedia.org/wikipedia/commons/5/59/Rutube_2012_logo.svg";
+export const category = [
+  { name: "New", icon: <Home /> },
+  { name: "Video", icon: <OndemandVideo /> },
+  { name: "Live", icon: <LiveTv /> },
+  { name: "Gaming", icon: <SportsEsports /> },
+  { name: "Education", icon: <School /> },
+  { name: "Sport", icon: <FitnessCenter /> },
+  { name: "Comedy", icon: <TheaterComedy /> },
+  { name: "Podcast", icon: <GraphicEq /> },
+  { name: "Fashion", icon: <Checkroom /> },
+  { name: "Crypto", icon: <DeveloperMode /> },
+  { name: "Gym", icon: <FitnessCenter /> },
+  { name: "Beauty", icon: <FaceRetouchingNatural /> },
+  { name: "Music", icon: <MusicNote /> },
+];
